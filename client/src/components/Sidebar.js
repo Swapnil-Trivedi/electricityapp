3a67a422e30af6c877e4b9828cb72006ed3b91fc
Swapnil-Trivedi <swapniltrivedi1456@gmail.com>
2022-07-13 10:38:28 +0530

    added signout feature

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,7 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Sidebar() {
+
+  let navigate=useNavigate();
+  const handleSignout=()=>{
+    localStorage.clear();
+    console.log("Cleared everything loggin out");
+    navigate("/");
+  }
+
   return (
     <>
       <div className="offcanvas offcanvas-start" tabIndex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel" style={{marginTop:"58px",maxWidth: "250px"}}>
@@ -13,8 +21,9 @@ function Sidebar() {
         </div>
         <div className="offcanvas-body">
           <div>
-                <ul className="sidebar-item"><Link to="/Dashboard"><i className="fa fa-chalkboard mx-3 my-2"></i>  Dashboard </Link></ul>
-                <ul className="sidebar-item"><Link to="/MyDetails"><i className="fa-solid fa-user-check mx-3 my-2"></i>  My Details </Link></ul>
+                <ul className="sidebar-item" data-bs-dismiss="offcanvas"><Link to="/Dashboard"><i className="fa fa-chalkboard mx-3 my-2"></i>  Dashboard </Link></ul>
+                <ul className="sidebar-item" data-bs-dismiss="offcanvas"><Link to="/MyDetails"><i className="fa-solid fa-user-check mx-3 my-2"></i>  My Details </Link></ul>
+                <ul className="sidebar-item" data-bs-dismiss="offcanvas" onClick={handleSignout}><i className="fa-solid fa-power-off mx-3 my-2"></i>  Sign Out</ul>
           </div>
         </div>
       </div>
